9430aebe1f785b038f8d55c5c49bb489d80c7932
ReDMallard <marco@titori.it>
2023-12-17 23:35:46 +0100

    done

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World Php</title>
+    <?php $nome = "Ciao mondo php!" ?>
+    <?php $nome2 = "Benvenuto!" ?>
+</head>
+<body>
+    <h1><?php echo $nome . " primo esercizio" ?></h1> 
+    <h1><?php echo "$nome2 " ?></h1>
+</body>
+</html>
